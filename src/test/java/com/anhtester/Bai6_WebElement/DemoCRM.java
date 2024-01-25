8c7c4c8431eb4de5684338c2eedacc513e2e5a17
Anh Tester <thaian.it15@gmail.com>
2024-01-25 19:18:12 +0700

    Bài 6 - WebElement - full

--- src/test/java/com/anhtester/Bai6_WebElement/DemoCRM.java
+++ src/test/java/com/anhtester/Bai6_WebElement/DemoCRM.java
@@ -25,7 +25,7 @@ public class DemoCRM {
 
         driver.findElement(By.xpath("//input[@id='password']")).sendKeys("123456");
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+        driver.findElement(By.xpath("//button[normalize-space()='Login']")).submit();
 
         //isSelected
         driver.findElement(By.xpath("//div[normalize-space()='Dashboard Options']")).click();
@@ -34,6 +34,31 @@ public class DemoCRM {
         Thread.sleep(1000);
         System.out.println("Checkbox 2: " + driver.findElement(By.xpath("//input[@id='widget_option_finance_overview']")).isSelected());
 
+        driver.findElement(By.xpath("//span[normalize-space()='Customers']")).click();
+
+        //Get Text
+        System.out.println(driver.findElement(By.xpath("//span[normalize-space()='Customers Summary']")).getText());
+
+        //Get Attribute
+        System.out.println(driver.findElement(By.xpath("//input[@class='form-control input-sm']")).getAttribute("placeholder"));
+
+        //Get Css value (color)
+        System.out.println(driver.findElement(By.xpath("//span[@class='text-success tw-truncate sm:tw-text-clip']")).getCssValue("color"));
+        System.out.println(driver.findElement(By.xpath("//span[@class='text-success tw-truncate sm:tw-text-clip']")).getCssValue("font-size"));
+
+        System.out.println(driver.findElement(By.xpath("//span[normalize-space()='Inactive Customers']")).getCssValue("color"));
+        System.out.println(driver.findElement(By.xpath("//span[normalize-space()='Inactive Customers']")).getCssValue("font-size"));
+
+        System.out.println(driver.findElement(By.xpath("//a[normalize-space()='New Customer']")).getCssValue("background-color"));
+        System.out.println(driver.findElement(By.xpath("//a[normalize-space()='New Customer']")).getCssValue("width"));
+        System.out.println(driver.findElement(By.xpath("//a[normalize-space()='New Customer']")).getCssValue("height"));
+        System.out.println(driver.findElement(By.xpath("//a[normalize-space()='New Customer']")).getText());
+
+        //Get Size of button
+        System.out.println(driver.findElement(By.xpath("//a[normalize-space()='New Customer']")).getSize());
+
+        //Get Location
+        System.out.println(driver.findElement(By.xpath("//a[normalize-space()='New Customer']")).getLocation());
 
         Thread.sleep(2000);
         driver.quit();
